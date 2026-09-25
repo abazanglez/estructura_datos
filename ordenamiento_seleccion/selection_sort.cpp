c940bc2f7dd9c79ad3197934786262e280375cd8
abazanglez <abazanglez@gmail.com>
2026-09-25 11:23:13 -0600

    feat: agrega contadores de comparaciones e intercambios y vectores para casos de prueba

--- ordenamiento_seleccion/selection_sort.cpp
+++ ordenamiento_seleccion/selection_sort.cpp
@@ -1,48 +1,79 @@
 #include <iostream>
+#include <vector>
 #include <utility>
 using namespace std;
 
-void mostrarArreglo(const int numeros[], int n)
+void mostrarArreglo(const vector<int>& numeros)
 {
-    for (int i = 0; i < n; i++)
+    for (size_t i = 0; i < numeros.size(); i++)
     {
         cout << numeros[i] << " ";
     }
     cout << endl;
 }
 
-void selectionSort(int numeros[], int n)
+void selectionSort(vector<int> numeros)
 {
+    int n = numeros.size();
+    int comparaciones = 0;
+    int intercambios = 0;
+
+    cout << "\nArreglo original:" << endl;
+    mostrarArreglo(numeros);
+    cout << endl;
+
     for (int i = 0; i < n - 1; i++)
     {
         int minIdx = i;
         for (int j = i + 1; j < n; j++)
         {
+            comparaciones++;
             if (numeros[j] < numeros[minIdx])
             {
                 minIdx = j;
             }
         }
+
         if (minIdx != i)
         {
             swap(numeros[i], numeros[minIdx]);
+            intercambios++;
         }
 
         cout << "Pasada " << (i + 1) << ":" << endl;
-        mostrarArreglo(numeros, n);
+        mostrarArreglo(numeros);
     }
+
+    cout << "\n========================================" << endl;
+    cout << "       ESTADISTICAS DE EJECUCION" << endl;
+    cout << "========================================" << endl;
+    cout << "Comparaciones hechas: " << comparaciones << endl;
+    cout << "Intercambios hechos: " << intercambios << endl;
 }
 
 int main()
 {
-    int numeros[] = {64, 45, 32, 20, 12, 8, 3};
-    int n = 7;
+    vector<vector<int>> casos = {
+        {8, 3, 7, 4, 2, 9, 1, 6, 5},
+        {1, 2, 3, 4, 5, 6, 7, 8, 9},
+        {9, 8, 7, 6, 5, 4, 3, 2, 1},
+        {1, 2, 3, 5, 4, 6, 7, 8, 9},
+        {847, 132, 596, 421, 73, 905, 268, 714, 359, 51, 683, 194, 772, 315, 638, 27, 481, 856, 203, 749, 564, 118, 932, 377, 690, 245, 813, 96, 527, 341, 765, 182, 604, 459, 88, 721, 296, 547, 11, 879, 403, 156, 634, 285, 998, 367, 529, 62, 746, 213, 591, 824, 174, 438, 957, 326, 705, 39, 681, 251, 519, 903, 147, 612, 334, 778, 225, 864, 93, 476, 542, 719, 184, 653, 309, 987, 71, 395, 829, 263, 608, 116, 451, 936, 278, 697, 33, 583, 762, 149, 875, 224, 514, 68, 946, 382, 727, 197, 556, 811},
+        {394, 827, 156, 643, 72, 918, 235, 501, 769, 43, 682, 314, 957, 128, 846, 291, 574, 9, 735, 462, 803, 177, 629, 354, 996, 215, 488, 761, 92, 537, 681, 346, 815, 264, 598, 731, 119, 425, 884, 57, 623, 308, 947, 186, 516, 793, 68, 371, 659, 242, 899, 137, 782, 453, 326, 704, 21, 561, 839, 275, 614, 983, 105, 478, 352, 726, 191, 875, 41, 667, 298, 549, 912, 164, 387, 755, 81, 528, 694, 333, 973, 224, 607, 146, 438, 869, 52, 781, 317, 645, 253, 590, 708, 98, 413, 954, 182, 521, 611, 886},
+        {621, 84, 937, 315, 476, 152, 803, 27, 694, 548, 371, 916, 203, 765, 439, 58, 827, 286, 603, 119, 972, 341, 714, 65, 529, 881, 247, 398, 756, 173, 634, 22, 845, 462, 709, 134, 953, 317, 581, 96, 428, 773, 251, 607, 862, 44, 695, 328, 517, 789, 186, 934, 73, 556, 402, 918, 145, 671, 263, 849, 36, 725, 491, 608, 157, 883, 294, 532, 761, 113, 647, 354, 905, 218, 779, 61, 583, 427, 996, 324, 716, 189, 875, 47, 638, 267, 540, 812, 98, 453, 729, 174, 590, 335, 941, 126, 684, 256, 817, 369}
+    };
 
-    cout << "Arreglo original:" << endl;
-    mostrarArreglo(numeros, n);
-    cout << endl;
-
-    selectionSort(numeros, n);
+    int opcion;
+    cout << "SELECCIONA EL CASO DE PRUEBA A EJECUTAR (1 a 7): ";
+    if (cin >> opcion && opcion >= 1 && opcion <= 7)
+    {
+        selectionSort(casos[opcion - 1]);
+    }
+    else
+    {
+        cout << "Opcion invalida. Ejecutando Caso 1 por defecto." << endl;
+        selectionSort(casos[0]);
+    }
 
     return 0;
 }
